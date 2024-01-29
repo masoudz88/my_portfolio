fef742c718a36eee5cfdbf87abf947bd4b0df5e9
Masoud Zare <masoudzare@Masouds-MacBook-Pro.local>
2024-01-29 11:03:44 -0330

    make about page

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div style={{ textAlign: 'center', margin: '20px' }}>
+      <h1>About Me</h1>
+      <p>I'm a passionate software developer with experience in web and mobile application development...</p>
+    </div>
+  );
+};
+
+export default About;
