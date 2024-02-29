6267f8aa332f2f07ad2c8a86eff213b966334bf7
Masoud Zare <masoudzare@Masouds-MacBook-Pro.local>
2024-02-29 15:16:42 -0330

    minor fix for responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { styles } from "../styles";
 
 const Hero = () => {
   return (
-    <section className={`relative w-full h-[75vh] mx-auto`}>
+    <section className={`relative w-full h-[85vh] mx-auto`}>
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
